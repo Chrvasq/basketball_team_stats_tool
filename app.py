54721eebfffbb3a8458dc0299b06ed7a896877c4
Chris Vasquez <chrvasq@outlook.com>
2019-05-18 19:45:59 -0700

    Implement clean_data function

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-from constants import PLAYERS, TEAMS
+import constants
+from copy import deepcopy
+
+teams_list = constants.TEAMS
+players_list = deepcopy(constants.PLAYERS)
+
+
+def clean_data():
+    for player in players_list:
+        player['guardians'] = player['guardians'].split(' and ')
+        player['experience'] = True if player['experience'] == 'YES' else False
+        player['height'] = int(player['height'][:2])
+    return players_list
+
+
+def balance_team():
+    pass
 
 
 if __name__ == '__main__':
-    pass
+    clean_data()
